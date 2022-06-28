900f8cacc39d9d4f7bf33caebb713f60424f1c41
Rainey Schafer <schafer.rainey@gmail.com>
2022-06-28 14:17:13 -0500

    Add joke app, hides and displays the punchline

---
+++ react_joke/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Joke from "./Joke";
+import jokesData from "./jokesData";
+
+function App() {
+  const jokeElements = jokesData.map((joke) => {
+    return <Joke key={joke.id} setup={joke.setup} punchline={joke.punchline} />;
+  });
+  return <div>{jokeElements}</div>;
+}
+
+export default App;
