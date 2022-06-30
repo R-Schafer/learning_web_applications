3e62b99db2c827af906e8f43889427527282c705
Rainey Schafer <schafer.rainey@gmail.com>
2022-06-30 12:45:14 -0500

    Add meme generator

---
+++ react_meme_generator/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="header">
+      <img
+        src="./images/troll-face.png"
+        alt="grinning face"
+        className="header--image"
+      />
+      <h2 className="header--title">Meme Generator</h2>
+    </header>
+  );
+}
+
+export default Header;
