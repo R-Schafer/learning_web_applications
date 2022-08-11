fc6d28b54658864c078a269d17a16f4593e2f484
Rainey Schafer <schafer.rainey@gmail.com>
2022-08-11 16:13:45 -0500

    Added functinality to +-*/=

--- js_calculator/script.js
+++ js_calculator/script.js
@@ -1,43 +1,134 @@
 const input = document.querySelector("#display");
 const keys = document.querySelectorAll(".keys");
-const equals = document.querySelector("#equals");
-let STATE = 0;
+let STATE = [0, 0, ""];
 
 function main() {
   keys.forEach((key) => key.addEventListener("click", handleKeyClick));
-
-  equals.addEventListener("click", handleEquals);
 }
 
 function handleKeyClick(e) {
   e.preventDefault();
-  if (e.target.innerHTML < 10) {
+  if (parseInt(e.target.innerHTML, 10) < 10) {
+    if (STATE[2] !== "" && STATE[1] === 0) {
+      input.value = "";
+    }
     input.value += e.target.innerHTML;
     updateState(parseInt(input.value, 10));
   }
   if (e.target.innerHTML === "C") {
-    updateState(e.target.innerHTML);
+    updateState("clear");
   }
   if (e.target.innerHTML === "⌫") {
     updateState("del");
   }
+  if (e.target.innerHTML === "+") {
+    updateState("add");
+  }
+  if (e.target.innerHTML === "-") {
+    updateState("sub");
+  }
+  if (e.target.innerHTML === "×") {
+    updateState("mul");
+  }
+  if (e.target.innerHTML === "÷") {
+    updateState("div");
+  }
+  if (e.target.innerHTML === ".") {
+    updateState("dec");
+  }
+  if (e.target.innerHTML === "+/-") {
+    updateState("neg");
+  }
+  if (e.target.innerHTML === "=") {
+    updateState("equals");
+  }
+  console.log(STATE);
 }
 
 function updateState(value) {
-  if (STATE >= 0) {
-    STATE = value;
+  if (value === "add") {
+    if (STATE[2] !== "") {
+      calc();
+    }
+    STATE[2] = "+";
   }
-  if (value === "C") {
-    STATE = 0;
+  if (value === "sub") {
+    if (STATE[2] !== "") {
+      calc();
+    }
+    STATE[2] = "-";
   }
-  if (value === "del") {
-    STATE = input.value.slice(0, -1);
+  if (value === "mul") {
+    if (STATE[2] !== "") {
+      calc();
+    }
+    STATE[2] = "*";
   }
-  input.value = STATE;
+  if (value === "div") {
+    if (STATE[2] !== "") {
+      calc();
+    }
+    STATE[2] = "/";
+  }
+  if (typeof value === "number") {
+    if (STATE[2] === "") {
+      STATE[0] = value;
+    } else {
+      STATE[1] = value;
+    }
+  }
+  if (value === "clear") {
+    STATE = [0, 0, ""];
+  }
+  // if (value === "del") {
+  //   STATE = input.value.slice(0, -1);
+  //   if (STATE.length === 0) {
+  //     STATE = 0;
+  //   }
+  // }
+
+  // if (value === "dec") {
+  //   STATE[0] * STATE[1];
+  // }
+  // if (value === "neg") {
+
+  // }
+
+  if (value === "equals") {
+    calc();
+  }
+
+  render();
+}
+
+function calc() {
+  if (STATE[2] === "+") {
+    STATE = [STATE[0] + STATE[1], 0, ""];
+  }
+  // subtract
+  if (STATE[2] === "-") {
+    STATE = [STATE[0] - STATE[1], 0, ""];
+  }
+  // multiply
+  if (STATE[2] === "*") {
+    STATE = [STATE[0] * STATE[1], 0, ""];
+  }
+  // divide
+  if (STATE[2] === "/") {
+    STATE = [STATE[0] / STATE[1], 0, ""];
+  }
+}
+
+function render() {
+  if (STATE[2] === "" || STATE[1] === 0) {
+    input.value = STATE[0];
+  } else {
+    input.value = STATE[1];
+  }
 }
 
 function handleEquals() {
-  input.value = 80085;
+  render();
 }
 
 main();
