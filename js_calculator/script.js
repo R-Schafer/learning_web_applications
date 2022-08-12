125a160296e8c929c5137d7262203f0b9043aa7a
Rainey Schafer <schafer.rainey@gmail.com>
2022-08-12 13:46:23 -0500

    Switched input to div, added functionality to del, neg, dec

--- js_calculator/script.js
+++ js_calculator/script.js
@@ -1,6 +1,6 @@
-const input = document.querySelector("#display");
+const display = document.querySelector(".display");
 const keys = document.querySelectorAll(".keys");
-let STATE = [0, 0, ""];
+let STATE = ["", "", ""];
 
 function main() {
   keys.forEach((key) => key.addEventListener("click", handleKeyClick));
@@ -8,92 +8,110 @@ function main() {
 
 function handleKeyClick(e) {
   e.preventDefault();
-  if (parseInt(e.target.innerHTML, 10) < 10) {
-    if (STATE[2] !== "" && STATE[1] === 0) {
-      input.value = "";
+
+  if (parseInt(e.target.innerHTML, 10) < 10 || e.target.innerHTML === ".") {
+    // for clearing the display right before user puts in 2nd number
+    if (STATE[1] !== "" && STATE[2] === "") {
+      display.innerHTML = "";
     }
-    input.value += e.target.innerHTML;
-    updateState(parseInt(input.value, 10));
+    display.innerHTML += e.target.innerHTML;
+    updateState(display.innerHTML);
   }
+
   if (e.target.innerHTML === "C") {
     updateState("clear");
   }
-  if (e.target.innerHTML === "⌫") {
+
+  if (e.target.innerHTML === "⏎") {
     updateState("del");
   }
+
   if (e.target.innerHTML === "+") {
     updateState("add");
   }
+
   if (e.target.innerHTML === "-") {
     updateState("sub");
   }
+
   if (e.target.innerHTML === "×") {
     updateState("mul");
   }
+
   if (e.target.innerHTML === "÷") {
     updateState("div");
   }
-  if (e.target.innerHTML === ".") {
-    updateState("dec");
-  }
+
   if (e.target.innerHTML === "+/-") {
     updateState("neg");
   }
+
   if (e.target.innerHTML === "=") {
     updateState("equals");
   }
-  console.log(STATE);
 }
 
 function updateState(value) {
-  if (value === "add") {
-    if (STATE[2] !== "") {
-      calc();
+  // operator
+  if (STATE[0] !== "") {
+    if (value === "add") {
+      if (STATE[1] !== "" && STATE[2] !== "") {
+        calc();
+      }
+      STATE[1] = "+";
     }
-    STATE[2] = "+";
-  }
-  if (value === "sub") {
-    if (STATE[2] !== "") {
-      calc();
+    if (value === "sub") {
+      if (STATE[1] !== "" && STATE[2] !== "") {
+        calc();
+      }
+      STATE[1] = "-";
     }
-    STATE[2] = "-";
-  }
-  if (value === "mul") {
-    if (STATE[2] !== "") {
-      calc();
+    if (value === "mul") {
+      if (STATE[1] !== "" && STATE[2] !== "") {
+        calc();
+      }
+      STATE[1] = "*";
     }
-    STATE[2] = "*";
-  }
-  if (value === "div") {
-    if (STATE[2] !== "") {
-      calc();
+    if (value === "div") {
+      if (STATE[1] !== "" && STATE[2] !== "") {
+        calc();
+      }
+      STATE[1] = "/";
     }
-    STATE[2] = "/";
   }
-  if (typeof value === "number") {
-    if (STATE[2] === "") {
+  // number or decimal
+  if (/^[+-]?[0-9]*\.?[0-9]*$/.test(value)) {
+    if (STATE[1] === "") {
       STATE[0] = value;
     } else {
-      STATE[1] = value;
+      STATE[2] = value;
     }
   }
+
+  // clear
   if (value === "clear") {
-    STATE = [0, 0, ""];
+    STATE = ["", "", ""];
   }
-  // if (value === "del") {
-  //   STATE = input.value.slice(0, -1);
-  //   if (STATE.length === 0) {
-  //     STATE = 0;
-  //   }
-  // }
 
-  // if (value === "dec") {
-  //   STATE[0] * STATE[1];
-  // }
-  // if (value === "neg") {
+  // backspace
+  if (value === "del") {
+    if (STATE[1] === "") {
+      STATE[0] = display.innerHTML.slice(0, -1);
+    } else {
+      STATE[2] = display.innerHTML.slice(0, -1);
+    }
+  }
 
-  // }
+  // negative or positive
+  if (value === "neg") {
+    if (STATE[1] === "") {
+      STATE[0] = STATE[0] * -1;
+    } else {
+      STATE[2] = STATE[2] * -1;
+    }
+  }
 
+  // equals
   if (value === "equals") {
     calc();
   }
@@ -102,33 +120,30 @@ function updateState(value) {
 }
 
 function calc() {
-  if (STATE[2] === "+") {
-    STATE = [STATE[0] + STATE[1], 0, ""];
+  // add
+  if (STATE[1] === "+") {
+    STATE = [(parseFloat(STATE[0]) + parseFloat(STATE[2])).toString(), "", ""];
   }
   // subtract
-  if (STATE[2] === "-") {
-    STATE = [STATE[0] - STATE[1], 0, ""];
+  if (STATE[1] === "-") {
+    STATE = [(parseFloat(STATE[0]) - parseFloat(STATE[2])).toString(), "", ""];
   }
   // multiply
-  if (STATE[2] === "*") {
-    STATE = [STATE[0] * STATE[1], 0, ""];
+  if (STATE[1] === "*") {
+    STATE = [(parseFloat(STATE[0]) * parseFloat(STATE[2])).toString(), "", ""];
   }
   // divide
-  if (STATE[2] === "/") {
-    STATE = [STATE[0] / STATE[1], 0, ""];
+  if (STATE[1] === "/") {
+    STATE = [(parseFloat(STATE[0]) / parseFloat(STATE[2])).toString(), "", ""];
   }
 }
 
 function render() {
-  if (STATE[2] === "" || STATE[1] === 0) {
-    input.value = STATE[0];
+  if (STATE[1] === "" || STATE[2] === "") {
+    display.innerHTML = STATE[0];
   } else {
-    input.value = STATE[1];
+    display.innerHTML = STATE[2];
   }
-}
-
-function handleEquals() {
-  render();
 }
 
 main();
