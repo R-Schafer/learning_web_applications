2a677b495c7fdd80065cafc31f308da52cdbcebd
Rainey Schafer <schafer.rainey@gmail.com>
2022-08-24 12:33:43 -0500

    Added date input, fetch error, and renamed functions

--- js_nasa/script.js
+++ js_nasa/script.js
@@ -1,24 +1,36 @@
 const title = document.querySelector(".title");
-const img = document.querySelector("img");
-const credit = document.querySelector(".img_credit");
+const date = document.querySelector("#date");
+const img = document.querySelector("#picture_of_day");
 const description = document.querySelector(".description");
 
-async function fetchNASA() {
-  const response = await fetch(`/nasa/planetary/apod`);
-  return response.json();
+function main() {
+  // prohibits user from clicking future date
+  date.max = new Date().toISOString().split("T")[0];
+  // listens for the date to change
+  date.addEventListener("change", gatherInfo);
+
+  gatherInfo();
 }
 
-async function start() {
-  const data = await fetchNASA();
+async function gatherInfo() {
+  const archived = date.value;
+  const data = await fetchNASA(archived);
   render(data);
+}
+
+async function fetchNASA(archived) {
+  const response = await fetch(`/nasa/planetary/apod?date=${archived}`);
+  if (!response.ok) {
+    throw new Error("failed to fetch");
+  }
+  return response.json();
 }
 
 function render(data) {
   title.textContent = data.title;
   img.src = data.url;
+  img.alt = data.title;
   description.textContent = data.explanation;
-  credit.textContent = `Image Credit & Copright: ${data.copyright}`;
-  console.log(data);
 }
 
-start();
+main();
